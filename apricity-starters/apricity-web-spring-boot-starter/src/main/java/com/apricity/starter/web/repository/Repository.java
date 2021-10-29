e2d702bcf9ba869da0253edcf9ba902bf6f586b3
liusong <treeshade_liu@163.com>
2021-10-29 23:46:05 +0800

    delete

--- apricity-starters/apricity-web-spring-boot-starter/src/main/java/com/apricity/starter/web/repository/Repository.java
+++ apricity-starters/apricity-web-spring-boot-starter/src/main/java/com/apricity/starter/web/repository/Repository.java
@@ -1,21 +1,4 @@
 package com.apricity.starter.web.repository;
 
-import com.apricity.starter.web.repository.provider.MybatisProvider;
-import org.apache.ibatis.annotations.InsertProvider;
-import org.apache.ibatis.annotations.SelectProvider;
-
-import java.util.Collection;
-import java.util.List;
-
 public interface Repository<T> {
-    @InsertProvider()
-    boolean insert(T t);
-
-    int insertBatch(Collection<T> entities);
-
-    @SelectProvider(MybatisProvider.class)
-    List<T> listAll();
-
-    @SelectProvider(MybatisProvider.class)
-    T selectById();
 }
