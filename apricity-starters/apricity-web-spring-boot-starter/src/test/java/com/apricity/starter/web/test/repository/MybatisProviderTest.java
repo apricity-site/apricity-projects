4dd3ce5dfde0c5ff38653141e23602a6fb3be353
apricity <1213380440@qq.com>
2021-11-18 22:19:41 +0800

    单元测试 init

--- apricity-starters/apricity-web-spring-boot-starter/src/test/java/com/apricity/starter/web/test/repository/MybatisProviderTest.java
+++ apricity-starters/apricity-web-spring-boot-starter/src/test/java/com/apricity/starter/web/test/repository/MybatisProviderTest.java
@@ -1,10 +1,22 @@
 package com.apricity.starter.web.test.repository;
 
 
+
 import com.apricity.starter.web.test.SpringBaseTest;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = SpringMybatisConfig.class)
+@ContextConfiguration(classes = {SpringMybatisConfig.class})
 public class MybatisProviderTest extends SpringBaseTest {
-    
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    void t00_providerInjectTest() {
+        Configuration configuration = sqlSessionFactory.getConfiguration();
+    }
 }
--- apricity-starters/apricity-web-spring-boot-starter/src/test/java/com/apricity/starter/web/test/repository/SpringMybatisConfig.java
+++ apricity-starters/apricity-web-spring-boot-starter/src/test/java/com/apricity/starter/web/test/repository/SpringMybatisConfig.java
@@ -1,6 +1,16 @@
 package com.apricity.starter.web.test.repository;
 
 
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.extension.MybatisMapWrapperFactory;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -13,11 +23,23 @@ import javax.sql.DataSource;
 import java.io.IOException;
 
 public class SpringMybatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        Configuration configuration = new Configuration();
+        configuration.setJdbcTypeForNull(JdbcType.NULL);
+        /* 驼峰转下划线 */
+        configuration.setMapUnderscoreToCamelCase(true);
+        /* map 下划线转驼峰 */
+        configuration.setObjectWrapperFactory(new MybatisMapWrapperFactory());
+        return  new DefaultSqlSessionFactory(configuration);
+    }
+
     @Bean("dataSource")
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
         dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost:3306/web_app_test?useSSL=false&useUnicode=true&characterEncoding=UTF-8");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test?useSSL=false&useUnicode=true&characterEncoding=UTF-8");
         dataSource.setUsername("root");
         dataSource.setPassword("123456");
         return dataSource;
